1c687bba907cb5ca088932f28bf2be00d9a51001
lethwombie <lethalwombat@outlook.com>
2022-12-20 17:37:09 +1000

    stock code data

---
+++ dash_app/stock_codes.py
@@ -0,0 +1,26 @@
+stock_codes = [
+    {
+        'name' : 'IVV',
+        'description' : 'iShares Core S&P 500 ETF',
+    },
+    {
+        'name' : 'IJR',
+        'description' : 'iShares Core S&P Small-Cap ETF',
+    },
+    {
+        'name' : 'AAPL',
+        'description' : 'Apple Inc',
+    },
+    {
+        'name' : 'BTC-USD',
+        'description' : 'Bitcoin USD',
+    },
+    {
+        'name' : 'VAS.AX',
+        'description' : 'Vanguard Australian Shares Index Fund',
+    },
+    {
+        'name' : 'VAP.AX',
+        'description' : 'Vanguard Australian Property Securities Index Fund',
+    }        
+]
